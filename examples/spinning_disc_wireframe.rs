8e4830f8213fd4dee3452cde04532dde91c4bd4a
Niklas Cathor <niklas.cathor@gmx.de>
2024-03-13 10:07:18 +0100

    Add example using the `wireframe` feature

---
+++ examples/spinning_disc_wireframe.rs
@@ -0,0 +1,68 @@
+use bevy::{prelude::*, utils::Duration};
+use bevy_stl::StlPlugin;
+use core::f32::consts::PI;
+
+fn main() {
+    App::new()
+        .insert_resource(Msaa::Sample4)
+        .add_plugins(DefaultPlugins)
+        .add_plugins(StlPlugin)
+        .insert_resource(SpinTimer(Timer::from_seconds(1.0 / 60.0, TimerMode::Repeating)))
+        .add_systems(Startup, setup)
+        .add_systems(Update, spin_disc)
+        .run();
+}
+
+#[derive(Component)]
+struct Disc {
+    angle: f32,
+}
+
+#[derive(Resource)]
+struct SpinTimer(Timer);
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        PbrBundle {
+            mesh: asset_server.load("models/disc.stl#wireframe"),
+            material: materials.add(Color::rgb(0.9, 0.4, 0.3)),
+            transform: Transform::from_rotation(Quat::from_rotation_z(0.0)),
+            ..Default::default()
+        },
+        Disc { angle: 0.0 }
+    ));
+    commands.spawn(PointLightBundle {
+        transform: Transform::from_xyz(30.0, 0.0, 20.0),
+        point_light: PointLight {
+            range: 40.0,
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_translation(Vec3::new(0.0, -100.0, 100.0))
+            .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
+        ..Default::default()
+    });
+}
+
+fn spin_disc(
+    time: Res<Time>,
+    mut timer: ResMut<SpinTimer>,
+    mut query: Query<(&mut Disc, &mut Transform)>,
+) {
+    if timer
+        .0
+        .tick(Duration::from_secs_f32(time.delta_seconds()))
+        .just_finished()
+    {
+        for (mut disc, mut transform) in query.iter_mut() {
+            disc.angle = disc.angle + 0.3 * PI / 180.0;
+            *transform = Transform::from_rotation(Quat::from_rotation_z(disc.angle));
+        }
+    }
+}
